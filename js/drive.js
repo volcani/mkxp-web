2c8df35ec7c199bb014dd42ca7d37accb0c8d2a1
pulsejet <radialapps@gmail.com>
2020-10-19 06:49:10 +0000

    Deploying to built from  @ 2c08238259c15ea808e9009c73dbd18130756521 🚀

---
+++ js/drive.js
@@ -0,0 +1,20 @@
+// https://stackoverflow.com/a/9458996
+function _bytesToBase64(bytes) {
+    var binary = '';
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
+// https://stackoverflow.com/a/21797381
+function _base64ToBytes(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes;
+}
